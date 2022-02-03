62dea2b9f288bbe21ece31ea2a0ec1cc5733266c
Ron Winchester <rom0203@gmail.com>
2022-02-03 21:10:23 +0300

    feat: создание сервера

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
+const router = require('./routes/products') 
+
+const app = express();
+app.use(express.json());
+
+app.use('/', router);
+
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
