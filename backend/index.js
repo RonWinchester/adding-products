c37dbaaa13f7aac1623aec37912078b7ba667580
Ron Winchester <rom0203@gmail.com>
2022-02-04 21:46:38 +0300

    fix: cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const { PORT = 5000 } = process.env;
 const mongoose = require('mongoose');
 const router = require('./routes/products') 
-
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use('/', router);
