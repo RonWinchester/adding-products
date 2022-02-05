ab53909a438825552d20b57d6dd3f7128f9316dd
Ron Winchester <rom0203@gmail.com>
2022-02-05 19:23:00 +0300

    feat: футер, компонент контента

--- frontend/src/components/Content/Content.js
+++ frontend/src/components/Content/Content.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Elements from "../Elements/Elements";
+import SideBar from "../SideBar/SideBar";
 
 function Content () {
     return (
         <div className="content">
             <Elements/>
+            <SideBar/>
         </div>
     )
 }
