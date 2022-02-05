238db8249c0620108ffeb3a29d3980b88d5fec90
Ron Winchester <rom0203@gmail.com>
2022-02-05 19:25:51 +0300

    feat: новые стили и api

--- frontend/src/utils/api/Api.js
+++ frontend/src/utils/api/Api.js
@@ -1,13 +1,48 @@
-import { getResponseData, apiUrl } from "../../constants/constants"
+import { getResponseData, apiUrl } from "../../constants/constants";
 
 export function getProduct() {
-    return fetch(`${apiUrl}`, {
-    method: 'GET',
+  return fetch(`${apiUrl}`, {
+    method: "GET",
     headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-    }
-    }).then((res)=>{
-        return getResponseData(res)
-    })
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    return getResponseData(res);
+  });
+}
+
+export function movieSearch(query) {
+  return fetch(`${apiUrl}product`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: query,
+    }),
+  }).then((res) => {
+    return getResponseData(res);
+  });
+}
+
+export function createProduct(data) {
+   const {title, description} = data
+  return fetch(`${apiUrl}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        title,
+        description,
+        params: {
+          type: data.params.type,
+          brand: data.params.brand,
+        }}),
+  }).then((res) => {
+    return getResponseData(res);
+  });
 }
