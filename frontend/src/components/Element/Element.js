0d4914196c05c4abd67b1b6e30cbba8387f748ef
Ron Winchester <rom0203@gmail.com>
2022-02-05 13:41:29 +0300

    feat: elements компоненты

--- frontend/src/components/Element/Element.js
+++ frontend/src/components/Element/Element.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Element ({title, description}) {
+function Element ({title, description, params}) {
+
     return (
-        <li>
-            <h2>{title}</h2>
-            <p>{description}</p>
+        <li className="element">
+            <h2 className="element__title">{title}</h2>
+            <p className="element__description">{description}</p>
+            <ul className="element__params">
+                {Object.keys(params).slice(0,-1).map((element, index)=>(
+                    <li className="element__setting" key={index}>{element}: {params[element]}</li>
+                ))}
+            </ul>
         </li>
     )
 }
--- frontend/src/components/Elements/Elements.js
+++ frontend/src/components/Elements/Elements.js
@@ -3,14 +3,13 @@ import { Context } from "../../contexts/context";
 import Element from "../Element/Element";
 
 function Elements() {
-  const { product } = React.useContext(Context);
-  console.log(product);
+  const  product  = React.useContext(Context);
   return (
-    <ul>
-      {product !== undefined ? (
-        product.map((element) => <Element key={element._id} title={element.title} description={element.description}/>)
+    <ul className="elements">
+    {product.length > 0 ? (
+        product.map((element) => <Element key={element._id} title={element.title} description={element.description} params={element.params}/>)
       ) : (
-        <p>Нет товаров</p>
+        <p className="elements__plug">Нет товаров</p>
       )}
     </ul>
   );
