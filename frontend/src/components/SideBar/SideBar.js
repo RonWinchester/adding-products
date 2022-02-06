1891d3850e57ff6b0b654b3144137f44beb5b5aa
Ron Winchester <rom0203@gmail.com>
2022-02-06 17:52:49 +0300

    feat: SideBar 1.0

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -3,49 +3,65 @@ import { Context } from "../../contexts/context";
 
 function SideBar() {
   const context = React.useContext(Context);
-  let set = [];
+  const [checked, setChecked] = React.useState(null)
+  function chengeCheckbox(e) {
+    setChecked(e.target.value);
+    console.log(e.target.value)
+ }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    console.log()
+  }
+
+  let onlyParams = [];
+  let paramsName = [];
+  let types = new Set();
+  let brands = new Set();
+
   context.forEach((element) => {
-    set.push(element.params);
-  });
-  let params = [];
-
-  let params1 = new Set();
-  let params2 = new Set();
-
-  set.forEach((element) => {
-    params = Object.fromEntries(Object.entries(element).slice(0, -1));
-    params1.add(Object.values(element).slice(0, -1)[0])
-    params2.add(Object.values(element).slice(0, -1)[1])
-    
-    /* params.forEach((el)=>{
-      Array.from(el)
-    }) */
-
+    onlyParams.push(element.params);
   });
 
-  console.log(params)
+  onlyParams.forEach((element) => {
+    paramsName = Object.keys(element).slice(0, -1);
+    types.add(Object.values(element).slice(0, -1)[0]);
+    brands.add(Object.values(element).slice(0, -1)[1]);
+  });
+
+  let allSpecifications = [Array.from(types), Array.from(brands)];
   return (
     <form className="sidebar">
       <h2 className="sidebar__title">Фильтр</h2>
-      {/* <ul className="element__settings">
-        {params.length > 0 ? (
-          params.map((element, index) => (
-            <li className="element__setting" key={index}>
-              {element}
-              <ul className="element__settings">
-                {params.map((el, indx) => (
-                  <li key={indx}>
-                    <input type="radio" />
-                    <label htmlFor="radio">{el}</label>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))
-        ) : (
-          <p>Нет фильтров</p>
-        )}
-      </ul> */}
+      {
+        <ul className="element__settings">
+          {paramsName.length > 0 ? (
+            paramsName.map((element, index) => (
+              <li className="element__setting" key={index}>
+                {element}
+                <ul className="element__settings">
+                  {allSpecifications[index].map((el, indx) => (
+                    <li
+                      key={indx}
+                      className="element__setting element__setting_nested"
+                    >
+                      <label htmlFor="checkbox" className="sidebar__label">
+                        <input type="checkbox" className="sidebar__input" value={el} name={el} checked={el === checked} onChange={(e)=> chengeCheckbox(e)}/>
+                        {el}
+                      </label>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))
+          ) : (
+            <p>Нет фильтров</p>
+          )}
+        </ul>
+      }
+      <button type="submit" className="sidebar__button" onClick={handleSubmit}>
+        Применить
+      </button>
     </form>
   );
 }
